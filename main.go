9cf1ff4671d62b24f417e926b657826b54646e15
andy.zx <andy.zx@alibaba-inc.com>
2017-03-10 01:25:17 +0800

    初始化

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	_ "milksoftweb/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
+
---
+++ routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"milksoftweb/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
